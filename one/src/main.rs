f6cb3c264bcac599ff2bf36fd066e14501d295c2
5CYTH3 <ScytheD@outlook.fr>
2022-12-03 15:38:57 +0100

    Added part 2 of Day 1

--- one/src/main.rs
+++ one/src/main.rs
@@ -2,14 +2,18 @@ use std::fs;
 
 fn main() {
     let content = fs::read_to_string("input.txt").expect("It should work bro");
-    let parsed = parse(content);
-    println!("{:?}", parsed)
+    let higher_calories = calculate_higher_calories(content.clone());
+    println!("{}", higher_calories);
+
+    let top_three = calculate_top_three(content);
+
+    println!("{:?}", top_three);
 }
 
 // temp; if != "": counter += line, else: if temp <= counter: temp = counter;
 // While the line isn't blank, we add up parsed numbers to the counter. If the counter is higher than the
 // current temporary variable, then we replace it's value by the value of the counter.
-fn parse(input: String) -> i64 {
+fn calculate_higher_calories(input: String) -> i64 {
     let mut temp: i64 = 0;
     let mut counter: i64 = 0;
 
@@ -25,4 +29,27 @@ fn parse(input: String) -> i64 {
         }
     }
     temp
+}
+
+fn calculate_top_three(input: String) -> (i64, i64, i64) {
+    let mut third: i64 = 0;
+    let mut second: i64 = 0;
+    let mut first: i64 = 0;
+    let mut counter: i64 = 0;
+
+    for line in input.lines() {
+        if line != "" {
+            let num = line.parse::<i64>().unwrap();
+            counter += num;
+        } else {
+            if first <= counter {
+                third = second;
+                second = first;
+                first = counter;
+            }
+            counter = 0;
+        }
+    }
+
+    (first, second, third)
 }
